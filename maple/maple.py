483c6fe54952a213e571c3007be409fa9d7f7e36
Euirim Choi <euirim@Euirims-MacBook-Pro-2.local>
2016-06-28 16:46:20 -0600

    enhanced project structure

---
+++ maple/maple.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""Maple: automatically summarizes given text using
+a modified version of the TextRank algorithm."""
+
+import sys
+
+def summarize(doc):
+    # process document
+    document = " ".join(doc.strip().split("\n"))
+    
+
+    
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    sys.exit(main())
